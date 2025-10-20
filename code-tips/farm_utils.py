d7709c48bbe5d0d0c72e47068f2420049a18df63
Xuyang Han <43256411+xuyangHan@users.noreply.github.com>
2025-10-20 15:40:58 -0400

    Update farm_utils.py

--- code-tips/farm_utils.py
+++ code-tips/farm_utils.py
@@ -1,18 +1,18 @@
-def till_column(n):
-    # Till n columns 
-    for _ in range(n):
-        for i in range(get_world_size()):
-            till()
-            move(North)
-        move(East)
-    for _ in range(n):
-        move(West)
+def farm_column(crop):
+	for i in range(get_world_size()):
+		if can_harvest():
+			harvest()
+			plant(crop)
+		move(North)
 
-
-def plant_crop_column(crop_type):
-    """Plant one full column of a specific crop"""
-    for _ in range(get_world_size()):
-        if can_harvest():
-            harvest()
-            plant(crop_type)
-        move(North)
+def plant_column(n, crop):
+	# Till n columns 
+	for _ in range(n):
+		for i in range(get_world_size()):
+			if (crop == Entities.Carrot):
+				till()
+			plant(crop)
+			move(North)
+		move(East)
+	for _ in range(n):
+		move(West)
